7d2470220a0e52889eb8ac22783427fa42c27231
Talha Ahmed Chowdhury <105747007+TalhaAhmedCho@users.noreply.github.com>
2025-09-20 14:01:21 +0600

    Create cw1.c

---
+++ 20-Sep-2025/cw1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main(){
+    printf("SL\t Product\t Quantity\t Price\n1\t Carrot\t \t 1KG\t\t 200tk\n");
+    printf("2\t Beans\t \t 2KG\t\t 250tk\n");
+    printf("3\t Chicken\t 500GM\t\t 500tk\n");
+    printf("4\t Tomato Sauce\t 500mL\t\t 600tk\n");
+}
