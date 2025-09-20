a5ef98c073f252faa244d40f8f4cc237965a6d84
Talha Ahmed Chowdhury <105747007+TalhaAhmedCho@users.noreply.github.com>
2025-09-20 14:00:47 +0600

    Create mam2.c

---
+++ 20-Sep-2025/mam2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main(){
+    printf("Hello World \nTalha");
+}
+
+
+/*
+\n - New Line
+\t - Tab/4Space
+\r - Carraige Return
+\a - Alert
+\\ - Backslash
+\" - Double Quote
+*/
