a69528df489270e9a7e89ee7864647c496b8c727
TalhaAhmedCho <tahchma@gmail.com>
2026-01-28 00:22:20 +0600

    Add implementation for area calculation based on user-provided radius

--- 20-Nov-2025/cw1.c
+++ 20-Nov-2025/cw1.c
@@ -1,3 +1,13 @@
-//
-// Created by tahch on 10-Nov-25.
-//
+#include <stdio.h>
+
+float calc(int radius) {
+    float Area = 3.1416 * radius * radius;
+    return Area;
+}
+
+int main() {
+    int radius;
+    printf("Enter Radius: ");
+    scanf("%d", &radius);
+    printf("The Area is: %f", calc(radius));
+}
