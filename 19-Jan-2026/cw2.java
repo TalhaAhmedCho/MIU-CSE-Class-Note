6a5a7e0a776af118e8e1521b9346b75eabceaf09
TalhaAhmedCho <tahchma@gmail.com>
2026-01-28 00:24:34 +0600

    Add cw2.java file with a program to calculate and display the square of a number

---
+++ 19-Jan-2026/cw2.java
@@ -0,0 +1,7 @@
+public class cw2 {
+    public static void main(String[] args) {
+        int num = 6;
+        int square = num * num;
+        System.out.println("The square of " + num + " is: " + square);
+    }
+}
