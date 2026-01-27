919b851eedfa87c376a7ed37f48f4b4bc530174e
TalhaAhmedCho <tahchma@gmail.com>
2026-01-28 00:25:22 +0600

    Remove prb1, prb2, prb3, and prb4 Java files and their corresponding class files

--- 19-Jan-2026/prb2.java
+++
@@ -1,7 +0,0 @@
-public class prb2 {
-    public static void main(String[] args) {
-        int num = 6;
-        int square = num * num;
-        System.out.println("The square of " + num + " is: " + square);
-    }
-}
--- 19-Jan-2026/prb3.java
+++
@@ -1,7 +0,0 @@
-public class prb3 {
-    public static void main(String[] args) {
-        double celsius = 25;
-        double fahrenheit = (celsius * 9/5) + 32;
-        System.out.println(celsius + " Celsius is equal to " + fahrenheit + " Fahrenheit.");
-    }
-}
