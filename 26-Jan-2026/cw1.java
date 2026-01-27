ac1c9ef7d3b4629b0b7f8aec417f56ad63968781
TalhaAhmedCho <tahchma@gmail.com>
2026-01-28 00:25:29 +0600

    Add cw1.java file with a program to print numbers from 1 to 5 using a while loop

---
+++ 26-Jan-2026/cw1.java
@@ -0,0 +1,10 @@
+public class cw1 {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("While Loop from 1 to 5:");
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
